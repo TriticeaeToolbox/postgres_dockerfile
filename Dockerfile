f6dd8af7974e79f4b30ba3f3683403b8403668de
David Waring <dev@davidwaring.net>
2020-10-07 11:36:56 -0400

    Remove init/t3.sh Databases are now created dynamically in the T3/Breedbase setup scripts

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM postgres:12.3
 
 ENV POSTGRES_PASSWORD=postgres
 ADD init/run.sh /docker-entrypoint-initdb.d
-ADD init/t3.sh /docker-entrypoint-initdb.d
 ADD sql /docker-entrypoint-initdb.d/
 
--- init/t3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    CREATE DATABASE cxgn_triticum WITH TEMPLATE breedbase;
-    CREATE DATABASE cxgn_avena WITH TEMPLATE breedbase;
-    CREATE DATABASE cxgn_hordeum WITH TEMPLATE breedbase;
-EOSQL
